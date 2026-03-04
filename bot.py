6d8ff1ae839f071245b8c750855fd5ebcb093560
ibraevtimur-bit <ibraev_timur@yahoo.com>
2026-03-04 10:30:04 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
 
+import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def start_handler(message: types.Message):
+    await message.answer("🌿 Добро пожаловать в Jungle Restaurant!")
+
+@dp.message_handler()
+async def echo_handler(message: types.Message):
+    await message.answer(f"Вы написали: {message.text}")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
